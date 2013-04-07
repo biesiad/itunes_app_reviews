28971f7495f3e8bc5c8e757b2b6b22d7538ae5fd
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2013-04-07 18:07:28 +0200

    Cleaning code

--- lib/app_store_reviews/reviews.rb
+++ lib/app_store_reviews/reviews.rb
@@ -1,5 +1,5 @@
 # encoding: UTF-8
-require_relative 'review_page'
+require_relative 'reviews_page'
 require 'httpclient'
 require 'open-uri'
 require 'open_uri_redirections'
@@ -16,10 +16,11 @@ module AppStoreReviews
     end
 
     def each
+      AppStoreReviews::log.debug("Loading reviews (store_id: #{store_id}, app_id: #{app_id})")
       page = 0
       while true
         page += 1
-        reviews = page_reviews(page)
+        reviews = reviews_page(page)
 
         break if reviews.empty?
 
@@ -29,11 +30,13 @@ module AppStoreReviews
       end
     end
 
-    def page_reviews(page)
-      url = "http://phobos.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?sortOrdering=4&onlyLatestVersion=false&sortAscending=true&pageNumber=#{(page - 1)}&type=Purple+Software&id=#{@app_id}"
+    def reviews_page(page_number)
+      url = "http://phobos.apple.com/WebObjects/MZStore.woa/wa/viewContentsUserReviews?sortOrdering=4&onlyLatestVersion=false&sortAscending=true&pageNumber=#{(page_number - 1)}&type=Purple+Software&id=#{@app_id}"
       f = open(url, "User-Agent" => "iTunes-iPhone/2.2 (2)", "X-Apple-Store-Front" => "#{@store_id}-1", :allow_safe_redirections => true)
       page_text = f.read
-      AppStoreReviews::ReviewPage.new(page_text, page).items
+      AppStoreReviews.log.debug("Loading page #{page_number} (store_id: #{store_id}, app_id: #{app_id})")
+      AppStoreReviews.log.debug(page_text)
+      AppStoreReviews::ReviewsPage.new(page_text).reviews
     end
   end
 end
--- lib/app_store_reviews/reviews_page.rb
+++ lib/app_store_reviews/reviews_page.rb
@@ -4,11 +4,11 @@ require 'rexml/document'
 include REXML
 
 module AppStoreReviews
-  class ReviewPage
+  class ReviewsPage
     LINK_SELECTOR = "Document > View > ScrollView > VBoxView > View > MatrixView > VBoxView > VBoxView > VBoxView"
 
     def initialize(page_content)
-      doc = Nokogiri::XML("")
+      doc = Nokogiri::XML(page_content)
       @links = doc.css(LINK_SELECTOR)
     end
 
@@ -16,10 +16,16 @@ module AppStoreReviews
       @links.reduce([]) do |reviews, current|
         review = parse(current)
         reviews << review if review
+        reviews
       end
     end
 
+    private
+
     def parse(link)
+      AppStoreReviews.log.debug("Parsing link")
+      AppStoreReviews.log.debug(link)
+
       node = link.css('TextView > SetFontStyle')
       info = node[2].content.split(" - ")
       version = info[1].strip()[-5, 5]
@@ -31,24 +37,23 @@ module AppStoreReviews
       title_el = node[0].css('b')
       title = title_el.first.content
 
-      text = node[3].content.strip
+      text = node[3].content.strip.gsub(/(\s){2,}/, " ")
 
       node = link.css('HBoxView > HBoxView > HBoxView')
-      rating = node.attr('alt').value
-
-      print link if date.nil?
+      rating = node.attr('alt').value.delete(" Stars")
 
       {
         title: title,
         name: name,
-        text: text.gsub(/(\s){2,}/, " "),
-        rating: rating.delete(" Stars"),
+        text: text,
+        rating: rating,
         date: date,
         version: version
       }
 
     rescue StandardError => e
-      print e.message
+      AppStoreReviews.log.error("Error while parsing")
+      AppStoreReviews.log.error(e)
       nil
     end
   end
