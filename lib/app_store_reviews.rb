892d04f35f397b27677629a202d93e378707f1af
Grzegorz Biesiadecki <gbiesiadecki@gmail.com>
2013-04-07 18:06:43 +0200

    Added logger

--- lib/app_store_reviews.rb
+++ lib/app_store_reviews.rb
@@ -1,2 +1,11 @@
+require "logger"
 require "app_store_reviews/stores"
 require "app_store_reviews/reviews"
+
+module AppStoreReviews
+  @@log = Logger.new(NIL)
+  @@log.level = Logger::ERROR
+
+  def self.log=(log); @@log = log end
+  def self.log; @@log end
+end
